20e7f3e8b18284f1e2937047c4a0a857bd196693
IDRISSI-Karim <karim9idrissi@gmail.com>
2024-02-20 20:48:27 +0100

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchTracks } from './lib/fetchTracks';
 
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
@@ -10,13 +12,20 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
-const [trackIndex, setTrackIndex] = useState(0)
 
-const goToNextTrack = () => {
-  setTrackIndex(trackIndex + 1);
-}
 
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0)
+
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  }
+  const { data: tracks } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks
+  });
+  console.log (tracks);
+  tracks.length
   return (
     <div className="App">
       <header className="App-header">
@@ -25,11 +34,12 @@ const App = () => {
       </header>
       <div className="App-images">
         <p>Dojo2 Nantes</p>
-        <audio src={trackUrls[0]} autoPlay controls />
+        <p>Nombre de morceaux: {trackUrls.length}</p> 
         <audio src={trackUrls[trackIndex]} autoPlay controls />
         <button onClick={goToNextTrack}>
           Next track
         </button>
+      
       </div>
       <div className="App-buttons"></div>
     </div>
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,24 @@
-const apiToken: string = '';
+// const apiToken: string = '';
+
+// export const fetchTracks = async () => {
+//   // This function does nothing, yet
+// };
+
+const apiToken = 'BQBwxladnD35mQ6Bwx3GVEYx12BDHtSc6ywmV1igu-0wjaaD763FzMcCUSjLqgAMHUTn078x3lieAgxm6kQafysKdm870Cmm3GLsb2VzNJGoYrh7wGxR_zIpOKYquAi7ZhKmf6_G-jI4da5uKluLpDTrfF8PdkoeUV21HgX_DApYI56Gm1b7M-DI1-0JJGhmfzLo3H8BPJ18Qb7h6Wm4m5l1VEhbJQ';
+
+
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+     throw new Error(`Fetching tracks failed with status ${response.status}`)
+   }
+  const data = await response.json() as { items: unknown[] };
+
+  return data.items;
 };
